2681d32c89d4d7302aee8e4ab672f44bf399599d
Jay <jpca999@gmail.com>
2023-06-04 19:02:08 -0400

    -

--- src/AlgoEx_Easy/FindThreeLargestNumbers_17.java
+++ src/AlgoEx_Easy/FindThreeLargestNumbers_17.java
@@ -1,7 +1,10 @@
 package AlgoEx_Easy;
 
+// input = { 141, 1, 17,-7, -17, -27, 18, 541, 8, 7 , 7  }
+//output : sorted array of 3 largest nos;  [18, 141, 541]
 public class FindThreeLargestNumbers_17 {
     public static void main(String[] args) {
+
 
 
 
--- src/AlgoEx_Easy/Non_Constructible_Change_5.java
+++ src/AlgoEx_Easy/Non_Constructible_Change_5.java
@@ -2,12 +2,14 @@ package AlgoEx_Easy;
 
 //We are given an array of positive integers, which represent the values of coins
 // that we have in our possession. The array could have duplicates. We are asked
-// to write a function that returns the minimum amount of change that we cannot
+// to write a function that returns
+//
+// the minimum amount of change that we cannot
 // create with our coins. For instance, if the input array is [1, 2, 5], the
 // minimum amount of change that we cannot create is 4, since we
 // can create 1, 2, 3 (1 + 2) and 5.
 
 //https://github.com/pinglu85/algoExpert/blob/main/Easy/non-constructible-change.md
-
+//input = [5,7,1,1,2,3,22]
 public class Non_Constructible_Change_5 {
 }
